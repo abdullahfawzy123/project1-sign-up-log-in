46b4bc0f37683b4699b606f9bc14546392bf04bb
abdullah fawzy <abdullahfawzy2005@gmail.com>
2020-06-11 21:10:41 +0200

    third commit

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
-import requests
+import requests.
 
 def main():
-    res = request.get("https://www.goodreads.com/book/isbn/ISBN?format=FORMAT")
+    res = request.get("https://www.goodreads.com/book/review_counts.json ", params={"key": "rYlZz0UYSciZ9yOEPJpbA", "isbns": "0380795272"})
     if res.status_code != 200:
         raise Exception("ERROR")
     data = res.json()
